b26deb74f42052b633258deb61d53b37542e974a
josecruzio <josericardo.developer@gmail.com>
2020-07-14 00:39:42 +0100

    Learning a little bit of Swift :)

--- MemoryCard/ContentView.swift
+++ MemoryCard/ContentView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        return ZStack(content: {
+            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+            RoundedRectangle(cornerRadius: 10.0).stroke()
+            Text("👻")
+            
+        }).foregroundColor(Color.orange).padding()
+        
+        
     }
 }
 
