9e46b48984cdd13014815d22d72e065a6f50c783
josecruzio <josericardo.developer@gmail.com>
2020-07-13 23:53:19 +0100

    Initial Commit

---
+++ MemoryCard/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MemoryCard
+//
+//  Created by José Ricardo G cruz on 13/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
