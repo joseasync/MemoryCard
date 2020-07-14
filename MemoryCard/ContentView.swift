352dc871ecad42da518cb26d64a15a83da19e471
josecruzio <josericardo.developer@gmail.com>
2020-07-14 01:04:52 +0100

    Struct vision

--- MemoryCard/ContentView.swift
+++ MemoryCard/ContentView.swift
@@ -9,16 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        return ZStack(content: {
-            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-            RoundedRectangle(cornerRadius: 10.0).stroke()
-            Text("👻")
-            
-        }).foregroundColor(Color.orange).padding()
+        HStack {
+            ForEach(0..<4){ index in
+               CardView()
+            }
+        }
+            .foregroundColor(Color.orange)
+            .padding()
+            .font(Font.largeTitle)
         
         
     }
 }
+
+
+struct CardView: View{
+    var body: some View{
+        ZStack {
+            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+            RoundedRectangle(cornerRadius: 10.0).stroke()
+            Text("👻")
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
